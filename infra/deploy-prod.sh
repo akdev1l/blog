6f64926a33a3c04087d358a0bb052bad4eb58965
Alex Diaz <alex@akdev.xyz>
2023-04-09 22:14:50 -0400

    fix: remove tabs

---
+++ infra/deploy-prod.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+(
+    cd ../web
+    bundle exec jekyll build
+    cp -rva _site ../infra/_site
+    bundle exec jekyll clean
+)
+
+cdk synth -c BLOG_DOMAIN=blog.akdev.xyz
+cdk deploy -c BLOG_DOMAIN=blog.akdev.xyz
